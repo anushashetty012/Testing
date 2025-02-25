1f9bb48ac009238711e136309ee03b164f24c7b7
vishwas.prabhu <vishwas.prabhu@robosoftin.com>
2025-02-25 17:36:03 +0530

    Initialize project structure with configuration files and a password validation class

---
+++ Testing.java
@@ -0,0 +1,20 @@
+public class Testing {
+
+    public static boolean isValidPassword(String password) {
+        if (password == null || password.length() < 8) {
+            return false;
+        }
+        boolean hasUpper = false, hasLower = false, hasDigit = false, hasSpecial = false;
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) hasUpper = true;
+            else if (Character.isLowerCase(c)) hasLower = true;
+            else if (Character.isDigit(c)) hasDigit = true;
+            else hasSpecial = true;
+        }
+        return hasUpper && hasLower && hasDigit && hasSpecial;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
